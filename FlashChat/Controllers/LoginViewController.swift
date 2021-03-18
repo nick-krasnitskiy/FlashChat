602b26860819cb6380a08e9af6d119ebcfcbd3ac
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-03-18 08:56:48 +0300

    Login Screen UI was created.

---
+++ FlashChat/Controllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  FlashChat
+//
+//  Created by Nick Krasnitskiy on 18.03.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
