ce85ad3293c7a57e21da251c460efa2df235f07a
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-03-18 08:26:37 +0300

    Register  Screen UI was created.

---
+++ FlashChat/Controllers/RegisterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterViewController.swift
+//  FlashChat
+//
+//  Created by Nick Krasnitskiy on 17.03.2021.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
