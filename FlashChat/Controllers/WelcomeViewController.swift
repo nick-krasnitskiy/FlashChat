958ca092f1ae651d2656242adce7602a4b3168d6
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-03-18 15:47:47 +0300

    The typing animation of the title label was created.

--- FlashChat/Controllers/WelcomeViewController.swift
+++ FlashChat/Controllers/WelcomeViewController.swift
@@ -14,6 +14,19 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Create typing animation of the title label
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let text = "⚡️FlashChat"
+        for letter in text {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+            
+            
+        }
+        
     }
 
 
