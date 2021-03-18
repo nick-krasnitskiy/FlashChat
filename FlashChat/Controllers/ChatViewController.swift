4c5b74b84c75d10d974ef996b155e48272ae4dbc
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-03-18 09:50:09 +0300

    Chat Screen UI was created.

---
+++ FlashChat/Controllers/ChatViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatViewController.swift
+//  FlashChat
+//
+//  Created by Nick Krasnitskiy on 18.03.2021.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
