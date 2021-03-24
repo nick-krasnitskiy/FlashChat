7fdb8f84a98b8104cd4bf8e59209827735ea31b7
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-03-24 08:31:56 +0300

    Registering new users was added.

--- FlashChat/Controllers/RegisterViewController.swift
+++ FlashChat/Controllers/RegisterViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -15,6 +16,18 @@ class RegisterViewController: UIViewController {
     
     @IBAction func registerPressed(_ sender: UIButton) {
     
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    // Navigate to the ChatViewController
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+    
+        }
+        
     }
 
+}
 }
